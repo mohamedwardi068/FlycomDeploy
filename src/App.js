3e63dc9bdb37b601ac558047503d5c3c26df76a5
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-11-15 11:56:44 +0100

    final deployed version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./myComponents/Navbar";
 import Footer from "./myComponents/Footer";
@@ -10,17 +10,18 @@ import ProductsGrid from "./pages/ProductsGrid";
 import QualityGoods from "./myComponents/QualityGoods";
 import PopularProductsCarousel from "./myComponents/PopularProductsCarousel";
 
-
 import AdminPage from "./pages/AdminPage";
 import SponsorsMapGoogle from "./myComponents/SponsorsMapGoogle";
 
 const AppContent = () => {
   const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  // GitHub Pages uses hashes:  #/admin
+  const isAdminRoute = location.hash.startsWith("#/admin");
 
   return (
     <div className="App flex flex-col min-h-screen">
-
+      
       {/* Show Navbar only for public pages */}
       {!isAdminRoute && <Navbar />}
 
@@ -35,8 +36,7 @@ const AppContent = () => {
               <ProductsGrid />
               <QualityGoods />
               <PopularProductsCarousel />
-   
-              <SponsorsMapGoogle/>
+              <SponsorsMapGoogle />
             </>
           }
         />
